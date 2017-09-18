8a81d746f356a667870da77ccd1ceb32941e013b
ilyabogdanov <ilyab@7rays.ws>
2017-09-18 14:12:51 +0500

    updated vertical layout example to see stylesheet correction

--- src/examples/VerticalLayoutExample.jsx
+++ src/examples/VerticalLayoutExample.jsx
@@ -15,7 +15,9 @@ export default class VerticalLayoutExample extends React.Component {
                     <VerticalLayoutRow key={i} height={e+"px"} style={{background: "rgba(0,0,0,"+(0.01*e)+")"}}/>
                 ))}
                 <VerticalLayoutRow height="100%" style={{background: "white"}}/>
-                <VerticalLayoutRow height="2rem" style={{background: "rgba(0,0,0,0.34)"}}/>
+                <VerticalLayoutRow height="2rem" style={{background: "rgba(0,0,0,0.1)"}}/>
+                <VerticalLayoutRow padding="5px" style={{background: "rgba(0,0,0,0.1)"}}>no-height</VerticalLayoutRow>
+                <VerticalLayoutRow padding="5px" style={{background: "rgba(0,0,0,0.1)"}}>no-height</VerticalLayoutRow>
             </VerticalLayout>
         )
     }
